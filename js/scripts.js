75d98f1bdbb8898dfc6cf82f86798c72850dbe8e
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-05-27 15:30:59 +0300

    Change the value of favoriteFood and add to document

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+
+//pop up "Hello World" alert
+alert("Hello World!!");
+
+//print value of favoriteFood on document
+let favoriteFood = "spaghetti bolognese";
+document.write(favoriteFood);
+
+
+//Change the value of favoriteFood to Lasagna
+
+ favoriteFood = "Lasagna";
+ document.write("My favorite food is "+favoriteFood);
