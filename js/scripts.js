2d3d58bf2fd08fcce1d6e621bde96e783395aaed
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-05-28 19:49:05 +0300

    Added Pokemon array of Data to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 
-//pop up "Hello World" alert
-alert("Hello World!!");
 
-//print value of favoriteFood on document
-let favoriteFood = "spaghetti bolognese";
-document.write(favoriteFood);
-
-
-//Change the value of favoriteFood to Lasagna
-
- favoriteFood = "Lasagna";
- document.write("My favorite food is "+favoriteFood);
+//Array of Pokemnon data to display, assigned to pokemonList
+let pokemonList = [
+  {
+    name : 'Charmander',                               //Data on pokemon-1, Charmander
+    height : 2.00,
+    category : 'Lizard',
+    type : ['Fire'],
+    weaknesses : ['Water', 'Ground', 'Rock']
+  },
+  {
+    name : 'Charmeleon',                               //Data on pokemon-2, Charmeloen
+    height : 3.07,
+    category : 'Flame',
+    type : ['Fire'],
+    weaknesses : ['Ground', 'Rock', 'Water']
+  },
+  {
+    name : 'Wartortle',                                //Data on pokemon-3, Wartortle
+    height : 3.03,
+    category : 'Turtle',
+    type : ['Water'],
+    weaknesses : ['Grass','Electric']
+  },
+  {
+    name : 'Pikachu',                                 //Data on pokemon-4, Picachu
+    height : 1.04,
+    category: 'Mouse',
+    type : ['Electric'],
+    weaknesses : ['Ground']
+  }
+];
