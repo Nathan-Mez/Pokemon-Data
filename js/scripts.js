0978df7bd9b40f661432bcbf96613af024e586f2
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-05-29 08:25:42 +0300

    Changed the value of Heights in pokemonList in meters

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,28 @@
 let pokemonList = [
   {
     name : 'Charmander',                               //Data on pokemon-1, Charmander
-    height : 2.00,
+    height : 0.610,
     category : 'Lizard',
     type : ['Fire'],
     weaknesses : ['Water', 'Ground', 'Rock']
   },
   {
     name : 'Charmeleon',                               //Data on pokemon-2, Charmeloen
-    height : 3.07,
+    height : 1.090,
     category : 'Flame',
     type : ['Fire'],
     weaknesses : ['Ground', 'Rock', 'Water']
   },
   {
     name : 'Wartortle',                                //Data on pokemon-3, Wartortle
-    height : 3.03,
+    height : 0.991,
     category : 'Turtle',
     type : ['Water'],
     weaknesses : ['Grass','Electric']
   },
   {
     name : 'Pikachu',                                 //Data on pokemon-4, Picachu
-    height : 1.04,
+    height : 0.406,
     category: 'Mouse',
     type : ['Electric'],
     weaknesses : ['Ground']
