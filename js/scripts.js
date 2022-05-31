d226188624716c0e29e6286d26cd3d3e197d8c3d
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-05-31 11:38:08 +0300

    Wrapped the pokemonList in IIME & assigned to pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,52 @@
 
 
-//Array of Pokemnon data to display, assigned to pokemonList
-let pokemonList = [
-  {
-    name : 'Charmander',                               //Data on pokemon-1, Charmander
-    height : 0.610,
-    category : 'Lizard',
-    type : ['Fire'],
-    weaknesses : ['Water', 'Ground', 'Rock']
-  },
-  {
-    name : 'Charmeleon',                               //Data on pokemon-2, Charmeloen
-    height : 1.090,
-    category : 'Flame',
-    type : ['Fire'],
-    weaknesses : ['Ground', 'Rock', 'Water']
-  },
-  {
-    name : 'Wartortle',                                //Data on pokemon-3, Wartortle
-    height : 0.991,
-    category : 'Turtle',
-    type : ['Water'],
-    weaknesses : ['Grass','Electric']
-  },
-  {
-    name : 'Pikachu',                                 //Data on pokemon-4, Picachu
-    height : 0.406,
-    category: 'Mouse',
-    type : ['Electric'],
-    weaknesses : ['Ground']
-  }
-];
+/*Array of Pokemnon data to display, assigned to pokemonList
+wrapped in pokemonRepository to avoid accidentally accessing */
+let pokemonRepository = (function (){
+  let pokemonList = [
+    {
+      name : 'Charmander',                               //Data on pokemon-1, Charmander
+      height : 0.610,
+      category : 'Lizard',
+      type : ['Fire'],
+      weaknesses : ['Water', 'Ground', 'Rock']
+    },
+    {
+      name : 'Charmeleon',                               //Data on pokemon-2, Charmeloen
+      height : 1.090,
+      category : 'Flame',
+      type : ['Fire'],
+      weaknesses : ['Ground', 'Rock', 'Water']
+    },
+   {
+      name : 'Wartortle',                                //Data on pokemon-3, Wartortle
+      height : 0.991,
+      category : 'Turtle',
+      type : ['Water'],
+      weaknesses : ['Grass','Electric']
+    },
+    {
+      name : 'Pikachu',                                 //Data on pokemon-4, Picachu
+      height : 0.406,
+      category: 'Mouse',
+      type : ['Electric'],
+      weaknesses : ['Ground']
+    }
+  ];
+
+  return{
+    add : function(item) {
+      pokemonList.push(item);
+    },
+    getAll : function() {
+      return pokemonList;
+    }
+  };
+})();
+
 
 //List of pokemon names and their height displayed on document
-pokemonList.forEach(function(pokn){
+pokemonRepository.getAll().forEach(function(pokn){
   if (pokn.height > 1){
     document.write("Pokemon's name: "+pokn.name+", Height("+pokn.height+") - WOW! That's BIG<br>");
    }
