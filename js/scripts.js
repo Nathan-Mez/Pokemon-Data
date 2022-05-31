093ff34b496c9bd03b772b5cb83417e91ddb5062
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-05-31 11:53:38 +0300

    Changed return on pokemonRepository to object of same key value name

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,28 @@ wrapped in pokemonRepository to avoid accidentally accessing */
 let pokemonRepository = (function (){
   let pokemonList = [
     {
-      name : 'Charmander',                               //Data on pokemon-1, Charmander
+      name : 'Charmander',                            //Data on pokemon-1, Charmander
       height : 0.610,
       category : 'Lizard',
       type : ['Fire'],
       weaknesses : ['Water', 'Ground', 'Rock']
     },
     {
-      name : 'Charmeleon',                               //Data on pokemon-2, Charmeloen
+      name : 'Charmeleon',                            //Data on pokemon-2, Charmeloen
       height : 1.090,
       category : 'Flame',
       type : ['Fire'],
       weaknesses : ['Ground', 'Rock', 'Water']
     },
    {
-      name : 'Wartortle',                                //Data on pokemon-3, Wartortle
+      name : 'Wartortle',                            //Data on pokemon-3, Wartortle
       height : 0.991,
       category : 'Turtle',
       type : ['Water'],
       weaknesses : ['Grass','Electric']
     },
     {
-      name : 'Pikachu',                                 //Data on pokemon-4, Picachu
+      name : 'Pikachu',                             //Data on pokemon-4, Picachu
       height : 0.406,
       category: 'Mouse',
       type : ['Electric'],
@@ -34,15 +34,21 @@ let pokemonRepository = (function (){
     }
   ];
 
+  function add(item){
+    pokemonList.push(item);           //Adds item object to pokemonList array
+  }
+  function getAll(){                 //returns pokemonList array
+    return pokemonList;
+  }
+
   return{
-    add : function(item) {
-      pokemonList.push(item);
-    },
-    getAll : function() {
-      return pokemonList;
-    }
+    add : add,
+    getAll : getAll
   };
+
 })();
+//===================End of pokemonRepository============================================================
+
 
 
 //List of pokemon names and their height displayed on document
